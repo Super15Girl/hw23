0f3ec3c8f06acab9f091525796ce4f42751de0dd
Super15Girl <89168218+Super15Girl@users.noreply.github.com>
2024-08-29 13:59:04 +0300

    Update js.js

--- js.js
+++ js.js
@@ -112,7 +112,7 @@ const computers = [
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = date.getFullYear();
   
-    return ${day}.${month}.${year};
+    // return ${day}.${month}.${year};
   }
   
   const formattedString = formatDate(new Date("2022-01-01"));
@@ -125,7 +125,7 @@ const computers = [
     const hours = date.getHours();
     const minutes = date.getMinutes();
   
-    return саат ${hours}, минута ${minutes};
+    // return саат ${hours}, минута ${minutes};
   }
   
   const timeString = formatTime(new Date("2022-01-01T11:40:00"));
